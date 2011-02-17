c7b8986dee76d7c399b1bf56b767a4ca707139eb
Gregg Williams <gregg13@innerpaths.net>
2011-02-16 18:02:38 -0800

    initial commit; blank project

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject infwb "1.0.0-SNAPSHOT"
+  :description "an evolving, experimental workspace for manipulating infocards"
+  :main infwb.core
+  :dependencies [[org.clojure/clojure "1.3.0-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 		 [org.clojars.gw666/sxqj "beta2"]]
+  :dev-dependencies [[lein-run "1.0.0"]
+		     [swank-clojure "1.3.0-SNAPSHOT"]])
---
+++ src/infwb/core.clj
@@ -0,0 +1,1 @@
+(ns infwb.core)
