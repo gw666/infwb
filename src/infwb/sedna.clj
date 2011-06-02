13047f6bda9c04e86058b3e95eddecc9076fb871
Gregg Williams <gregg13@innerpaths.net>
2011-06-02 12:13:52 -0700

    WORKING: in all files, changed 'slip' to 'sldata', then 'sid' to 'slip'

--- src/infwb/sedna.clj
+++ src/infwb/sedna.clj
@@ -54,11 +54,11 @@
 
 (defn db-startup
   "does all database setup for current session of work; should be
-executed once; WARNING: deletes the database of icdatas and slips"
+executed once; WARNING: deletes the database of icdatas and sldatas"
   []
   ;WARNING - RE-EXECUTING THIS DELETES ICDATA DATABASE
   (def ^{:dynamic true} *icdata-idx*   0) ;;icdata db is 0th element of @*appdb*
-  (def ^{:dynamic true} *slip-idx*    1) ;;slip db is 1st element of @*appdb*
+  (def ^{:dynamic true} *sldata-idx*    1) ;;sldata db is 1st element of @*appdb*
   (def ^{:dynamic true} *appdb* (atom [{} {}]))
   
   (def ^{:dynamic true} *xqs* (SednaXQDataSource.)) ;naughty; OK for debugging
@@ -156,20 +156,20 @@ a working XQDataSource."
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
-; SLIPS: creating
+; SLDATAS: creating
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defrecord slip [sid      ;;string; id of slip
+(defrecord sldata [sid      ;;string; id of sldata
 		 icard     ;;string; card-id of icdata to be displayed
-		 pobj]   ;;Piccolo object that implements slip
+		 pobj]   ;;Piccolo object that implements sldata
   )
 
 (declare get-icdata)
 
-(defn new-slip
-  "create slip from infocard, with its pobj at (x y), or default to (0 0)--
-NOTE: does *not* add slip to *appdb*"
+(defn new-sldata
+  "create sldata from infocard, with its pobj at (x y), or default to (0 0)--
+NOTE: does *not* add sldata to *appdb*"
   ([icard x y]
   (let [icdata (get-icdata icard)
 	; this does nothing, for now
@@ -180,8 +180,8 @@ NOTE: does *not* add slip to *appdb*"
 		y
 		(icdata-field icdata :ttxt)
 		(icdata-field icdata :btxt))]
-    (slip. rand-key (atom icard) (atom pobj))))
-  ([icard]   (new-slip icard 0 0 )))
+    (sldata. rand-key (atom icard) (atom pobj))))
+  ([icard]   (new-sldata icard 0 0 )))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -206,7 +206,7 @@ NOTE: does *not* add slip to *appdb*"
 ; its icard record, use (get-icard iid); to get an icard record's fields,
 ; use (icard-field icard :fieldname).
 ;
-; NB: Slips are similar, with (get-slip sid), (slip-field slip :fieldname)
+; NB: Sldatas are similar, with (get-sldata sid), (sldata-field sldata :fieldname)
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -272,15 +272,15 @@ NOTE: does *not* add slip to *appdb*"
   []
   (count (keys (nth @*appdb* *icdata-idx*))))
 
-(defn slip->icdata
-  "given a slip id, return its icdata from the appdb"
-  [slip]
-  ;;the :icard field of the slip contains the id of the corresp. icdata
-  (get-icdata (:icard slip)))
+(defn sldata->icdata
+  "given a sldata id, return its icdata from the appdb"
+  [sldata]
+  ;;the :icard field of the sldata contains the id of the corresp. icdata
+  (get-icdata (:icard sldata)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
-; APPDB: populating it with slips    
+; APPDB: populating it with sldatas    
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -291,145 +291,145 @@ NOTE: does *not* add slip to *appdb*"
 ;; multiple times, and thus should be free of side effects. Returns
 ;; the value that was swapped in.
 
-;; Explanations of two lines of code within slip->appdb:
+;; Explanations of two lines of code within sldata->appdb:
 
-;; `(swap! *appdb* assoc-in [*slip-idx* sid] slip)` is equiv to
-;; `(apply assoc-in <curr value of *appdb*> [*slip-idx* sid] slip)`,
+;; `(swap! *appdb* assoc-in [*sldata-idx* sid] sldata)` is equiv to
+;; `(apply assoc-in <curr value of *appdb*> [*sldata-idx* sid] sldata)`,
 ;; then assigning the new value back to *appdb*.
 
-;; Let KEY = `(nth *appdb* *slip-idx* sid)`, which is the key `sid`
-;; within the map for slips.
+;; Let KEY = `(nth *appdb* *sldata-idx* sid)`, which is the key `sid`
+;; within the map for sldatas.
 
-;; The above swap...assoc-in line takes the value `slip` and uses it
+;; The above swap...assoc-in line takes the value `sldata` and uses it
 ;; to *replace* the value associated with KEY. 
 
 ;; -----
-;; `(swap! *appdb* update-in [*slip-idx*] assoc sid slip)` is equiv to
-;; `(apply update-in <curr value of *appdb*> [*slip-idx*] assoc sid slip)`,
+;; `(swap! *appdb* update-in [*sldata-idx*] assoc sid sldata)` is equiv to
+;; `(apply update-in <curr value of *appdb*> [*sldata-idx*] assoc sid sldata)`,
 ;; then assigning the new value back to *appdb*.
 
-;; Let VAL = `(nth *appdb* *slip-idx*)`, which is the map for slips.
+;; Let VAL = `(nth *appdb* *sldata-idx*)`, which is the map for sldatas.
 
 ;; The above swap...update-in line performs the following action:
 
-;; `assoc VAL sid slip`, which *prepends* the key/value pair to VAL.
+;; `assoc VAL sid sldata`, which *prepends* the key/value pair to VAL.
 
-(defn slip->appdb
-  "Stores the slip record in the in-memory database--NOTE: new slip is
-inserted at the *front* of the map, *before* all existing slips"
-  [slip]
-  (let [sid (:sid slip)
-	id-exists?  (get-in @*appdb* [*slip-idx* sid])]
-    (if id-exists?   ;;if true, replaces existing; false adds new slip
-      (swap! *appdb* assoc-in [*slip-idx* sid] slip)
-      (swap! *appdb* update-in [*slip-idx*] assoc sid slip)))
+(defn sldata->appdb
+  "Stores the sldata record in the in-memory database--NOTE: new sldata is
+inserted at the *front* of the map, *before* all existing sldatas"
+  [sldata]
+  (let [sid (:sid sldata)
+	id-exists?  (get-in @*appdb* [*sldata-idx* sid])]
+    (if id-exists?   ;;if true, replaces existing; false adds new sldata
+      (swap! *appdb* assoc-in [*sldata-idx* sid] sldata)
+      (swap! *appdb* update-in [*sldata-idx*] assoc sid sldata)))
   nil)
 
-;; WARN: "bare" use of `:sid` to get data from within slip
-(defn icard->slip->appdb
-  "Given id (= icard), creates slip, adds slip to *appdb*; returns sid of new slip"
+;; WARN: "bare" use of `:sid` to get data from within sldata
+(defn icard->sldata->appdb
+  "Given id (= icard), creates sldata, adds sldata to *appdb*; returns sid of new sldata"
   [icard]
-  (let [slip (new-slip icard)
-	sid (:sid slip)]
-    (slip->appdb slip)
+  (let [sldata (new-sldata icard)
+	sid (:sid sldata)]
+    (sldata->appdb sldata)
     sid))
 
-(defn load-all-slips-to-appdb []
+(defn load-all-sldatas-to-appdb []
   (let [all-icards (db->all-icards)]
     (doseq [icard all-icards]
-      (icard->slip->appdb icard))))
+      (icard->sldata->appdb icard))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
-; ACCESSING SLIPS AND THEIR DATA
+; ACCESSING SLDATAS AND THEIR DATA
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn get-slip  ;; aka "lookup-slip" (from appdb)
-  "given its sid, retrieve a slip from the appdb"
+(defn get-sldata  ;; aka "lookup-sldata" (from appdb)
+  "given its sid, retrieve a sldata from the appdb"
   [sid]
-  (let [slip   (get-in @*appdb* [*slip-idx* sid])]
-    (if slip
-      slip
-      {:sid (str "ERROR: Slip '" sid "' is INVALID")
-       :icard (atom (str "ERROR: Slip '" sid "' is INVALID"))
+  (let [sldata   (get-in @*appdb* [*sldata-idx* sid])]
+    (if sldata
+      sldata
+      {:sid (str "ERROR: Sldata '" sid "' is INVALID")
+       :icard (atom (str "ERROR: Sldata '" sid "' is INVALID"))
        :pobj  (atom nil)} )))
 
 (defn appdb->all-sids
-  "return a seq of all the id values of the appdb slip database"
+  "return a seq of all the id values of the appdb sldata database"
   []
-    (keys (get-in @*appdb* [*slip-idx*])))
+    (keys (get-in @*appdb* [*sldata-idx*])))
 
-;; TODO needs a test in slips.clj
-(defn slip-field
-  "given slip, get value of field named field-key (e.g.,:cid)"
-  [slip field-key]
+;; TODO needs a test in sldatas.clj
+(defn sldata-field
+  "given sldata, get value of field named field-key (e.g.,:cid)"
+  [sldata field-key]
 ;  (swank.core/break)
   (cond   (= :sid field-key)
-	  (:sid slip)
+	  (:sid sldata)
 	  
 	  (contains? #{:icard :pobj} field-key)
-	  @(field-key slip) 
+	  @(field-key sldata) 
 	
 	  (contains? #{:ttxt :btxt} field-key)
-	  (let [icdata (get-icdata (slip-field slip :icard))] ;;executed for icdata fields
-;	  (let [icdata (get-icdata (:icard slip))] ;;executed for icdata fields
+	  (let [icdata (get-icdata (sldata-field sldata :icard))] ;;executed for icdata fields
+;	  (let [icdata (get-icdata (:icard sldata))] ;;executed for icdata fields
 	    (icdata-field icdata field-key))
 	
 	  ;; icards are "moved" by changing their transform
 	  ;; getXOffset, getYOffset access the transform's values directly,
 	  ;; eliminating need to xform local X, Y (always 0 0) to globl coords
 	  (= :x field-key)
-	  (let [pobj (slip-field slip :pobj)]
+	  (let [pobj (sldata-field sldata :pobj)]
 	    (.getXOffset pobj))
 
 	  (= :y field-key)
-	  (let [pobj (slip-field slip :pobj)]
+	  (let [pobj (sldata-field sldata :pobj)]
 	    (.getYOffset pobj))
 	  
 	  ))
 
-(defn slip-appdb-size
+(defn sldata-appdb-size
   "number of icards in the application's internal icdata db"
   []
-  (count (keys (nth @*appdb* *slip-idx*))))
+  (count (keys (nth @*appdb* *sldata-idx*))))
 
 (defn move-to
-  "move a slip's Piccolo infocard to a given location; returns: slip"
-  [slip   ^Float x   ^Float y]
-  (let [pobj   @(:pobj slip)
+  "move a sldata's Piccolo infocard to a given location; returns: sldata"
+  [sldata   ^Float x   ^Float y]
+  (let [pobj   @(:pobj sldata)
 	at1    (AffineTransform. 1. 0. 0. 1. x y)]
     (.setTransform pobj at1))
-  slip)
+  sldata)
   
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;
-; DISPLAYING SLIPS ON THE DESKTOP
+; DISPLAYING SLDATAS ON THE DESKTOP
 ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; InfWb is largely about slips. If a function does not say what it is 
-;; operating on, it is probably doing so on a slip. Omitting mention of 
-;; a slip in a function name is a way of keeping code succinct.
+;; InfWb is largely about sldatas. If a function does not say what it is 
+;; operating on, it is probably doing so on a sldata. Omitting mention of 
+;; a sldata in a function name is a way of keeping code succinct.
 
 (defn show
-  "display a slip at a given location in a given layer"
+  "display a sldata at a given location in a given layer"
   ; BUG: move-to moves the PClip but not its contents
-  [slip   x y   layer]
-  (let [_   (move-to slip x y)
-	pobj   (slip-field slip :pobj)]
+  [sldata   x y   layer]
+  (let [_   (move-to sldata x y)
+	pobj   (sldata-field sldata :pobj)]
     (.addChild layer pobj)))
 
 (defn show-seq
-  "display seq of slips, starting at (x y), using dx, dy as offset
-for each next slip to be displayed"
-  [slip-seq   x y   dx dy   layer]
+  "display seq of sldatas, starting at (x y), using dx, dy as offset
+for each next sldata to be displayed"
+  [sldata-seq   x y   dx dy   layer]
   (println "Reached show-seq, x y = " x " " y)
   (let [x-coords   (iterate #(+ % dx) x)
 	y-coords   (iterate #(+ % dy) y)
         layer-seq  (repeat layer)]
 ;    (dorun
-     (map show slip-seq x-coords y-coords layer-seq))
+     (map show sldata-seq x-coords y-coords layer-seq))
 ;    )
   )
--- test/infwb/test/slips.clj
+++ test/infwb/test/slips.clj
@@ -1,14 +1,16 @@
 ; project: github/gw666/infwb
 ; file: /test/infwb/test/slips
 
+; HISTORY:
+
 (ns infwb.test.slips
   (:use [infwb.infocard] :reload)
   (:use [infwb.sedna] :reload)
   (:use [infwb.core] :reload)
   (:use [clojure.test]))
 
-;; Required manual setup: Sedna must have a database named "brain".
-;; Database must have a copy of the file
+;; Required manual setup: Sedna must have a db named "text" with a collection
+;; called "test". Collection must have a copy of the file
 ;; "/Users/gw/Documents/99-IMPORTANT DOCUMENTS/permanent infocards, sch
 ;; ema v0.90/hofstadter, doidge.XML". The file's
 ;; lowest key is "gw667_090815161114586", and there should be 67 records.
@@ -33,106 +35,109 @@
 ;; 'GW notes on Clojure', topic 'PROPOSED PROCEDURE for using InfWb'
 
 (defn test-slips-setup []
-  (println "doing setup; all icards being loaded into local db")
+  (println "doing setup")
   (db-startup)
-  (let [icard-seq (db->all-icards)]
-    (doseq [card icard-seq]
+  (let [iid-seq (db->all-iids)]
+    (doseq [card iid-seq]
       (db->appdb card))
     ))
-
-(defn reset-slip-db
-  "Clears out the slip-db so as to enable another round of testing without
-having to run `(test-slips-setup)`, which is time-consuming, again"
-  []
-  (swap! *appdb* assoc-in [*slip-idx*] {})
-  nil)
   
+(deftest test-write-1-slip []
+	 (let [test-iid   "gw667_090815161114586"
+	       an-icard (db->icard test-iid)
+	       a-slip (icard->new-slip an-icard)
+	       slip-id (slip-field a-slip :id)
+	       _ (slip->appdb a-slip)	;new slip added to appdb
+	       slip-retrieved (lookup-slip slip-id)]
+	   (and
+	    (is (= "gw667_090815161114586"
+		   (:iid slip-retrieved) ))
+	    (is (= 1 (count (appdb->all-sids)))) )
+	   ))
 
+(deftest test-create-pobj []
+	 (let [slip-id (nth (appdb->all-sids) 0)
+	       x-position 100
+	       y-position 50
+	       pobj (slip-pobj (lookup-slip slip-id) x-position y-position)]
+	    (is (= (round-to-int x-position) (round-to-int (.getX pobj)))
+	    (is (= (round-to-int y-position) (round-to-int (.getY pobj)))
+	    ))))
 
-;; (test-slips-setup) should run before this fcn runs. This makes
-;; appdb slip db empty.
-;; At end of this fcn, appdb slip db has one entry based on first icard.
-(deftest test-make-1-slip
-  "Creates test suite's first slip, based on first icard"
-  []
-  (let [test-icard (nth (appdb->all-icards) 0)
-	test-ttxt (icdata-field (get-icdata test-icard) :ttxt)
-	test-slip (new-slip test-icard)
+(deftest test-add-pobj-to-appdb []
+	 (let [slip-id (nth (appdb->all-sids) 0)
+	       partial-slip (lookup-slip slip-id)
+	       x-position 100
+	       y-position 50
+	       pobj (slip-pobj (lookup-slip slip-id) x-position y-position)
+	       full-slip   (new-full-slip partial-slip pobj)]
+	   (slip->appdb full-slip)
+	   (is (= pobj (:pobj (lookup-slip slip-id)))) ))
+
+(deftest test-display-1-slip []
+	 (let [slip-id (nth (appdb->all-sids) 0)
+	       slip (lookup-slip slip-id)
+	       card (:pobj slip)]
+	   (.addChild layer1 card)
+	   (println "test-display-1-slip passes if card is visible in window")
+	   (is true)))
+
+(deftest test-display-all-slips []
+  (let [iid-seq (appdb->all-iids)
+	]
+    (doseq [iid iid-seq]
+      (let [icard (lookup-icard iid)
+	    slip (icard->new-slip icard)
+	    _   (slip->appdb slip)
+	    pobj (slip-pobj slip (rand-int 300) (rand-int 200))
+	    full-slip   (new-full-slip slip pobj)
+	    _   (slip->appdb full-slip)
+	       card (:pobj full-slip)]
+	   (.addChild layer1 card)
+	   ))
+    (println "test-display-all-slips passes if cards are visible in window")
+    (is true)))
+
+(deftest test-make-1-slip []
+  (let [test-iid "gw667_090815162059614"
+	test-ttxt "to label, categorize, and find precedents"
+	default-x   0
+	default-y   0
+	test-slip (new-slip test-iid)
 	_   (slip->appdb test-slip)]
-;    (swank.core/break)
-    (is (= test-ttxt (slip-field test-slip :ttxt)) ":ttxt field")
-    (is (= 1 (count (appdb->all-sids)))) ))
+    (is (= test-ttxt (slip-field test-slip :ttxt) ":ttxt field")) ))
 
-;; DEV: This must be modified whenever a field is added
-;; This test works on the first slip, which was created by (test-make-1-slip)
 (deftest test-slip-field []
-  (let [icard (nth (appdb->all-icards) 0)
-	sid (nth (appdb->all-sids) 0)
-	slip (get-slip sid)
-	icdata (get-icdata icard)
-	ttxt (icdata-field icdata :ttxt)
-	btxt (icdata-field icdata :btxt)]
-    (is (= sid (slip-field slip :sid)))
-    (is (= icard (slip-field slip :icard)))
-    (is (= ttxt (slip-field slip :ttxt)))
-    (is (= btxt (slip-field slip :btxt))) ))
-  
-;; This test works on the first slip, which was created by (test-make-1-slip)
-(deftest test-move-slip
-  "Moves an existing slip, then checks to see whether move worked by
-examining the pobj's x and y values (using `slip-field`)"
-  []
-  (let [slip-id (nth (appdb->all-sids) 0) ; get first slip in appdb
+  (let [slip-id (nth (appdb->all-sids) 0)
 	new-x   62
 	new-y  118
-	test-slip (get-slip slip-id)]
-    (move-to test-slip new-x new-y)
-    (is (= new-x (round-to-int (slip-field test-slip :x))) "x")
-    (is (= new-y (round-to-int (slip-field test-slip :y))) "y") ))
+	test-slip (new-slip slip-id)]
+    (position-to test-slip new-x new-y)
+    (is (= new-x (round-to-int (slip-field test-slip :getX)) "getX"))
+    (is (= new-y (round-to-int (slip-field test-slip :getY)) "getY")) ))
 
 (deftest test-show-1-slip []
   (println "\n### WARN: Be sure that layer1 is defined ###\n")
   (let [slip-id (nth (appdb->all-sids) 0) 
-	test-slip (get-slip slip-id)
-	test-x   50
-	test-y  150]
-    (println "test-show-1-slip succeeds if you see slip named '"
-	     (slip-field test-slip :ttxt) "' onscreen at ("
-	     test-x " " test-y ")\n")
-    (show test-slip test-x test-y layer1) ))
-
-;; this fcn creates a second slip
-(deftest test-make-slip-from-db []
-  (let [icard2 (nth (appdb->all-icards) 1)
-	sid2   (icard->slip->appdb icard2)
-	slip2   (get-slip sid2)]
-    (is (= icard2 (slip-field slip2 :icard)))
-    (is (= sid2 (slip-field slip2 :sid)))
-    (is (= (slip-field (get-slip "INVALID KEY") :icard)
-	   "ERROR: Slip 'INVALID KEY' is INVALID"))
-    ))
+	test-slip (new-slip slip-id)
+	test-x   51
+	test-y  149]
+    (println "test-show-1-slip succeeds if you see slip named"
+	     (slip-field test-slip :ttxt) "onscreen") ))
 
 	 
 
 
 (defn test-ns-hook []
-  (println "### Did you define needed variables (e.g., layer1)? ###")
-  (println "### Did you re-eval? '(ns infwb.test.slips ... )' ? ###\n")
-  ;; (test-slips-setup)
+  (println "### Did you define needed variables (e.g., layer1)? ###\n")
+  (test-slips-setup)
   ;; (test-write-1-slip)
   ;; (test-create-pobj)
   ;; (test-add-pobj-to-appdb)
   ;; (test-display-1-slip)
   ;; (test-display-all-slips)
-  (println "running (test-make-1-slip)")
   (test-make-1-slip)
-  (println "running (test-slip-field)")
   (test-slip-field)
-  (println "running (test-move-slip)")
-  (test-move-slip)
-  (println "running (test-show-1-slip)")
   (test-show-1-slip)
-  (println "running (test-make-slip-from-db)")
-  (test-make-slip-from-db)
   )
 
