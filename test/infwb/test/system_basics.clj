512134ff9fd311991615a7b2144ceb12c68bb00c
Gregg Williams <gregg13@innerpaths.net>
2012-01-14 23:00:59 -0800

    system_basics.clj tests work w/ lein & midje-mode lein midje infwb.test.system-basics

--- test/infwb/test/system_basics.clj
+++ test/infwb/test/system_basics.clj
@@ -16,12 +16,20 @@
 ;;
 ;; * Sedna must have a db named "brain" and an _empty_ collection named 'test'
 ;;
-;; * (def trio (-main)) has been run
+;; * (def trio (-main)) has been run  (not sure about this)
+
+;; NOTE: Many of the 'checks' (lines containing =>) in this file are
+;; wrapped with a 'let' clause that does a lot of calculation. This
+;; occurs because the interactive debugging package 'midje-mode' doesn't
+;; seem to work properly when it comes to the 'around' clause making
+;; its value available to the corresponding test. (Executing this
+;; file using 'lein midje' work just fine that way.) I made these
+;; changes just so I could test interactively using 'midje-mode'.
 
 
 ;; icard low-level tests
 
-#_(against-background
+(against-background
   
   [(before :contents
            (doall
@@ -74,7 +82,7 @@
 
 ; slip low-level tests
 
-#_(against-background
+(against-background
     
   [(before :contents
             (db/SYSload "four-notecards"))
@@ -82,12 +90,7 @@
    (around :facts
            (let [IGNORE       (db/SYSclear-all) ; new test; clear local data
                  IGNORE       (db/SYSsetup-InfWb "brain" "test") ;more setup
-                 test-icard   (nth (db/get-all-icards) 0)
-                 test-icdata  (db/localDB->icdata test-icard)
-                 test-btxt    (db/icdata-field test-icdata :btxt)
-                 test-ttxt    (db/icdata-field test-icdata :ttxt)
-                 test-sldata  (db/new-sldata test-icard)
-                 test-slip    (nth (db/get-all-slips) 0)
+                 
                  ]
              ?form ))
 
@@ -99,51 +102,54 @@
   (fact
     ; FCNS TESTED: get-all-icards, localDB->icdata, icdata-field,
     ;    get-all-slips, SYSsldata-field
-    (count (db/get-all-slips)) => 1
-    (db/SYSsldata-field test-sldata :slip) => test-slip
-    (db/SYSsldata-field test-sldata :icard) => test-icard
-    (db/SYSsldata-field test-sldata :btxt) => test-btxt
-    (db/SYSsldata-field test-sldata :ttxt) => test-ttxt
-    )
+    (let [test-icard   (nth (db/get-all-icards) 0)
+          test-icdata  (db/localDB->icdata test-icard)
+          test-btxt    (db/icdata-field test-icdata :btxt)
+          test-ttxt    (db/icdata-field test-icdata :ttxt)
+          test-sldata  (db/new-sldata test-icard)
+          test-slip    (nth (db/get-all-slips) 0)]
+      (count (db/get-all-slips)) => 1
+      (db/SYSsldata-field test-sldata :icard) => test-icard
+      (db/SYSsldata-field test-sldata :btxt) => test-btxt
+      (db/SYSsldata-field test-sldata :ttxt) => test-ttxt
+      (db/SYSsldata-field test-sldata :slip) => test-slip
+      ))
 
   )   ; close "against-background"
 
 ; ------------------------------------------------------
 
-#_(against-background
+;.;. Out of clutter find simplicity; from discord find harmony; in the
+;.;.                               ; middle of difficulty lies
+;.;.                               ; opportunity. -- Einstein
+(against-background                     ;bombs
     
   [(before :contents
            (doall
             (db/SYSsetup-InfWb "brain" "test")
-            (db/SYSload "four-notecards")))
+            (db/SYSload "four-notecards"))
+           )
    
-   (around :contents
-           (let [IGNORE       (db/SYSclear-all) ; new test; clear local data
-                 test-icard   (nth (db/get-all-icards) 0)
-                 test-icdata  (db/localDB->icdata test-icard)
-                 test-sldata  (db/new-sldata test-icard)
-                 test-slip    (nth (db/get-all-slips) 0)
-                 new-x   62
-                 new-y  118
-                 IGNORE       (db/move-to test-slip new-x new-y)
-                 new-sldata   (db/get-sldata test-slip)
-;                 IGNORE       (println new-sldata)
-                 ]
-             ?form ))
-
    (after :contents
           (db/SYSdrop "four-notecards"))
    ]
 
   
   (fact
-    ; FCNS TESTED: 
-    (db/round-to-int (db/SYSsldata-field new-sldata :x)) => new-x
-    (db/round-to-int (db/SYSsldata-field new-sldata :y)) => new-y 
-;    (db/round-to-int 118.2) => new-y 
-    )
+    ; FCNS TESTED: move-to, get-sldata, round-to-int, SYSsldata-field
+    (let [IGNORE       (db/SYSclear-all) ; new test; clear local data
+          test-icard   (nth (db/get-all-icards) 0)
+          test-icdata  (db/localDB->icdata test-icard)
+          test-sldata  (db/new-sldata test-icard)
+          test-slip    (nth (db/get-all-slips) 0)
+          new-x         62
+          new-y        118
+          IGNORE       (db/move-to test-slip new-x new-y)
+          new-sldata   (db/get-sldata test-slip)]
+      (db/round-to-int (db/SYSsldata-field new-sldata :x)) => new-x
+      (db/round-to-int (db/SYSsldata-field new-sldata :y)) => new-y ))
   
-  )   ; close "against-background"
+  )  ; close "against-background"
 
 ; ------------------------------------------------------
 
@@ -154,22 +160,12 @@
             (db/SYSsetup-InfWb "brain" "test")
             (db/SYSload "four-notecards")))
    
-   (around :checks   ;was :facts
+   (around :facts
            (let [IGNORE       (db/SYSclear-all) ; new test; clear local data
                  test-icard   (nth (db/get-all-icards) 0)
                  test-sldata  (db/new-sldata test-icard)
-                 test-slip    (nth (db/get-all-slips) 0)
+                 
 
-                 frame1    (PFrame.)
-                 canvas1   (.getCanvas frame1)
-                 layer1    (.getLayer canvas1)
-                 dragger   (PDragEventHandler.)
-                 IGNORE    (.setMoveToFrontOnPress dragger true)
-                 IGNORE    (.setPanEventHandler canvas1 nil)
-                 IGNORE    (.addInputEventListener canvas1 dragger)
-                 IGNORE    (println
-                            "Check the topmost window; it should show"
-                            "one slip")
                  ]
              ?form ))
 
@@ -180,16 +176,26 @@
           ]
 
 (fact
-    ; FCNS TESTED: show
-    (db/show test-slip 50 150 layer1) => nil)
+  ; FCNS TESTED: show
+  (let [test-slip    (nth (db/get-all-slips) 0)
+        frame1    (PFrame.)
+        canvas1   (.getCanvas frame1)
+        layer1    (.getLayer canvas1)
+        dragger   (PDragEventHandler.)
+        IGNORE    (.setMoveToFrontOnPress dragger true)
+        IGNORE    (.setPanEventHandler canvas1 nil)
+        IGNORE    (.addInputEventListener canvas1 dragger)
+        IGNORE    (println
+                   "Check the topmost window; it should show"
+                   "one slip")
+        ]    
+    (db/show test-slip 50 150 layer1) => nil))
 
   )   ; close "against-background"
 
 ; ------------------------------------------------------
 
-; 120113 not working--see comment where 'fact' is, below;
-; giving up for now
-#_(against-background
+(against-background
     
   [(before :contents
             (db/SYSload "four-notecards"))
@@ -208,10 +214,6 @@
                  test-icard   (nth (db/get-all-icards) 0)
                  test-sldata  (db/new-sldata test-icard)
                  test-slip    (nth (db/get-all-slips) 0)
-                 nicard2      "gw667_090815163031398"
-                 slip2        (db/icard->sldata->localDB nicard2)
-                 sldata2      (db/get-sldata slip2)
-;                 IGNORE   (println sldata2)
                  ]
              ?form ))
 
@@ -222,10 +224,13 @@
 ; For some reason, the fact below does not "see" test-card or sldata2, even
 ; though it should be available because of the bindings in 'around' stmt above
   (fact
-    ; FCNS TESTED: 
-  (println test-icard) => nil
-  (db/SYSsldata-field sldata2 :icard) => "gw667_090815163031398"
-  )
+    ; FCNS TESTED:
+    (let [icard2       (nth (db/get-all-icards) 1)
+          slip2        (db/icard->sldata->localDB icard2)
+          sldata2      (db/get-sldata slip2)]
+          
+      (db/SYSsldata-field sldata2 :icard) => "gw667_090815163031398"
+      ))
 
   )   ; close "against-background"
 
